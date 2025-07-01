763bb3bbf04f88fac64efbe12881fc551f4aa987
yogeswaran P A <yokesh0524@gmail.com>
2025-07-01 07:53:49 +0000

    commit

---
+++ models/stg_employees.sql
@@ -0,0 +1,27 @@
+{{
+    config(
+        materialized = 'incremental',
+        unique_key = 'EMPLOYEE_ID'
+    )
+}}
+
+SELECT
+    EMPLOYEE_ID,
+    FIRST_NAME,
+    LAST_NAME,
+    EMAIL,
+    PHONE_NUMBER,
+    HIRE_DATE,
+    JOB_ID,
+    SALARY,
+    COMMISSION_PCT,
+    MANAGER_ID,
+    DEPARTMENT_ID,
+    CURRENT_TIMESTAMP as LOAD_TIME
+from {{source('hr','employees')}}
+
+{% if is_incremental() %}
+
+where LOAD_TIME > (select coalesce(max(LOAD_TIME),'1900-01-01 00:00:00')from {{this}})
+
+{% endif %}
